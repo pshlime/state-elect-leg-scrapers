29c3d6a9cfc252dae803d6575e534bc8bd9d1bd6
Ivy Yip <ivysyip@gmail.com>
2025-02-23 23:16:19 -0500

    created prompt for openai api, scrape specific pages

--- WI/code/scrape_bills.py
+++ WI/code/scrape_bills.py
@@ -27,6 +27,8 @@ def scrape_bill(uuid, state, state_bill_id, session):
     response = requests.get(bill_url)
     tree = html.fromstring(response.content)
 
+    scrape_votes(uuid, state, state_bill_id, session, tree)
+
     metadata, link = scrape_bill_metadata(uuid, state, state_bill_id, session, tree, bill_url)
     # append_to_csv(uuid, session, state_bill_id, link)
     # write_file(uuid, "bill_metadata", metadata)
@@ -106,13 +108,6 @@ def scrape_bill_sponsors(uuid, state, state_bill_id, session, page):
     return sponsor_data
 
 def scrape_bill_history(uuid, state, state_bill_id, session, page):
-    # bill_json_url = (
-    #     "https://docs.legis.wisconsin.gov/{}/proposals/{}".format(session, state_bill_id)
-    # )
-
-    # response = requests.get(bill_json_url)
-    # tree = html.fromstring(response.content)
-
     history = page.xpath("//div[@class='propHistory']/table[@class='history']/tr")
 
     actions = []
@@ -143,6 +138,50 @@ def scrape_bill_history(uuid, state, state_bill_id, session, page):
 
     return bill_history_data
 
+def scrape_votes(uuid, state, state_bill_id, session, page):
+    history = page.xpath("//div[@class='propHistory']/table[@class='history']/tr")
+
+    voting_events = set()
+
+    # getting all links to references
+    for event in history:
+        event_description = "".join(event.xpath("./td[@class='entry']//text()"))
+
+        for regex, _ in motion_classifiers.items():
+            if re.match(regex, event_description):
+                reference = event.xpath("./td[@class='journal noprint']/a/@href")[0]
+
+                voting_events.add(reference)
+
+    for voting_event in voting_events:
+        url = voting_event
+        response = requests.get(url)
+        page = html.fromstring(response.content)
+
+        # checking for important votes
+        if page.xpath("//div[@class='qs_entry_']//a//@href"):
+            text = "".join(page.xpath("//div[@class='journals']//text()"))
+
+            full_page_url = ""
+            for link in page.xpath("//a//@href"):
+                if re.match("^/1995/related/journals/assembly/[^/.]+$", link):
+                    full_page_url = link
+
+            if full_page_url:
+                response = requests.get(BASE_URL + full_page_url)
+                full_page = html.fromstring(response.content)
+                present = "".join(full_page.xpath("//div[@class='qs_present_']//text()"))
+
+                file = open(f"{voting_event[-4:]}.txt", "w")
+                file.write(present+ "\n" + text)
+                file.close()
+
+                scrapeWithOpenAI(state_bill_id, file)
+                # os.remove(f"{voting_event[-4:]}.txt")
+
+            else:
+                raise TypeError("Present member found")
+
 def append_to_csv(uuid, session, bill_number, link):
     filename = "bills.csv"
     file_exists = os.path.isfile(filename)
@@ -159,10 +198,41 @@ def write_file(file_name, directory, data):
     with open(f'AZ/output/{directory}/{file_name}.json', 'w') as f:
         json.dump(data, f, indent=4)
 
+def scrapeWithOpenAI(bill_id, file):
+    congress = {
+        "H": "House",
+        "S": "Senate",
+        "A": "Assembly",
+    }
+    prompt = f"""Extract all voting-related questions for {congress[bill_id[0]]} Bill {bill_id[2:]} ({bill_id}) from this text.
+    For each question that resulted in 'Motion carried' or includes recorded votes, return the following structured information:
+
+    - The exact question text.
+    - The full list of 'Ayes': If 'Motion carried' is present without explicit votes, assume all present members voted "Aye."
+    If a roll-call vote is recorded, use the provided list.
+    - The full list of 'Noes' (if applicable).
+    - The list of absent or not voting members (if applicable).
+    - Confirm whether the motion was carried.
+    - At the beginning of the response, provide the full list of all present members at the time of voting,
+    labeled as 'All Present Members.' Do not repeat this list under each question.
+
+    The output should be structured as follows:
+
+    All Present Members: [List of all names]
+
+    [Question]
+    Ayes: [List of names]
+    Noes: [List of names, if applicable]
+    Absent or not voting: [List of names, if applicable]
+    Motion carried.
+    """
+
+    print(prompt)
+
 if __name__ == "__main__":
     test_uuid = "WI1995AB694"
     test_state = "WI"
-    test_bill_id = "SB125"
+    test_bill_id = "AB330"
     test_session = "1995"
 
     scrape_bill(test_uuid, test_state, test_bill_id, test_session)
