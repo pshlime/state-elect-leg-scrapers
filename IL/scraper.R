b351b5f2b563f914cb95a95b8d59e160c9bc5ce3
iivvvyyyyy <ivywangah@gmail.com>
2025-03-23 18:45:38 -0400

    Update scraper.R

--- IL/scraper.R
+++ IL/scraper.R
@@ -1,1 +1,22 @@
+# libraries for working with JSON data
+library(tidyverse)
+library(jsonlite)
+library(glue)
 
+# Cleanup functions
+rm(list = ls())
+gc()
+
+# Setting file paths
+DROPBOX_PATH <- '/path/to/your/93rd_GA_data' # Update to new CSV folder
+OUTPUT_PATH <- '93rd_GA/output' # Update the output path for new data
+
+# Reading data sets and combine into data frames
+
+# Cleaning data 
+
+# Identify the most recent action for each bill, which helps track the status of each bill
+
+# Combine data frames
+
+# Save the data
