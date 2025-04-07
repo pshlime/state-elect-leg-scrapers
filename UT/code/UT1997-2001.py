d9308c438c39bf551af3520b60ba6379e6719614
Joe Loffredo <joseph.r.loffredo@gmail.com>
2025-04-06 21:50:41 -0400

    update bill history

--- UT/code/UT1997-2001.py
+++ UT/code/UT1997-2001.py
@@ -2,11 +2,79 @@ import json
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import re
 from dateutil.parser import parse
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+
+# Utility functions
+def write_file(file_name, directory, data):
+    with open(f'output/{directory}/{file_name}.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+def setup_driver():
+    # Set up Chrome options
+    chrome_options = Options()
+
+    # Specify the download directory and enable automatic downloads
+    prefs = {
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing.enabled": True
+    }
+    chrome_options.add_experimental_option("prefs", prefs)
+
+    # Add headless options (if needed)
+    chrome_options.add_argument("--headless")  # Runs Chrome in headless mode (no UI)
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+
+    # Initialize the Selenium webdriver with Chrome options
+    # Using webdriver_manager to handle the ChromeDriver automatically
+    service = Service(ChromeDriverManager().install())  # This will install and manage the ChromeDriver
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    return driver
+
+def is_date(string, fuzzy=False):
+    """
+    Return whether the string can be interpreted as a date.
+
+    :param string: str, string to check for date
+    :param fuzzy: bool, ignore unknown tokens in string if True
+    """
+    if string in ["1st","2nd","3rd","4th","5th"]:
+        return False
+    try:
+        parse(string, fuzzy=fuzzy)
+        return True
+    except ValueError:
+        return False
+
+def convert_to_date(date_str):
+    str_to_return = ""
+    str_list = date_str.split("/")
+    #do year first
+    if int(str_list[-1]) > 25:
+        str_to_return += "19"+str_list[-1]
+    else:
+        str_to_return += "20"+str_list[-1]
+    str_to_return += "-"
+    #now add 0 before month/day if necessary and add that
+    date = ""
+    for item in str_list[0:-1]:
+        if len(item) == 1:
+            date += "0"+item
+        else:
+            date += item
+    str_to_return += date[0:2]+"-"+date[2:]
+    return str_to_return
+
+def date_to_yyyymmdd(date_str):
+    date_obj = datetime.strptime(date_str, "%m/%d/%y")
+    return date_obj.strftime("%Y-%m-%d")
 
 def get_bill_metadata_1997_2001(uuid, session_year, state_bill_id):
     """
@@ -51,42 +119,7 @@ def get_bill_metadata_1997_2001(uuid, session_year, state_bill_id):
     
     return bill_metadata, sponsors_output
 
-def is_date(string, fuzzy=False):
-    """
-    Return whether the string can be interpreted as a date.
-
-    :param string: str, string to check for date
-    :param fuzzy: bool, ignore unknown tokens in string if True
-    """
-    if string in ["1st","2nd","3rd","4th","5th"]:
-        return False
-    try: 
-        parse(string, fuzzy=fuzzy)
-        return True
-    except ValueError:
-        return False
-
-def convert_to_date(date_str):
-    str_to_return = ""
-    str_list = date_str.split("/")
-    #do year first
-    if int(str_list[-1]) > 25:
-        str_to_return += "19"+str_list[-1]
-    else: 
-        str_to_return += "20"+str_list[-1]
-    str_to_return += "-"
-    #now add 0 before month/day if necessary and add that
-    date = ""
-    for item in str_list[0:-1]:
-        if len(item) == 1:
-            date += "0"+item
-        else:
-            date += item
-    str_to_return += date[0:2]+"-"+date[2:]
-    return str_to_return
-
-
-def get_bill_history_1997_2001(session_year, state_bill_id):
+def get_bill_history_1997_2001(uuid, session_year, state_bill_id):
     """
     Retrieves the bill history from the status text file and extracts action dates,
     descriptions, and vote counts (if available).
@@ -94,37 +127,47 @@ def get_bill_history_1997_2001(session_year, state_bill_id):
     # Construct the URL for the status text file.
     status_url = f"https://le.utah.gov/~{session_year}/status/hbillsta/{state_bill_id}.txt"
     response = requests.get(status_url)
-    text_data = response.text
-    text_list = text_data.split()
-    sponsor = get_bill_metadata_1997_2001(session_year, state_bill_id)[1]
+    response_text = response.text
 
-    start = text_list.index(f"{sponsor[-2:]})")
-    date = []
-    action = []
-    act = []
-    for unit in text_list[start+1:]:
-        if is_date(unit):
-            date.append(convert_to_date(unit))
-            if act:
-                action.append(" ".join(act[:-1]))#cuts off the last thing bc idk what it is
-                act=[]
-        else:
-           act.append(unit)
-    
-    return date,action
+    # Regular expression to match dates and descriptions
+    date_desc_pattern = r"(\d{1,2}/\d{1,2}/\d{2})\s+([^\n]+)"
 
+    # Find all matches
+    matches = re.findall(date_desc_pattern, response_text)
+
+    # History log
+    actions = []
+
+    for match in matches:
+        date, action = match
+
+        actions.append({
+            "date": date_to_yyyymmdd(date),
+            "action": action.strip()  # Clean any extra spaces from the action
+        })
+
+    bill_history_data = {
+        "uuid": uuid,
+        "state": state,
+        "session": session,
+        "state_bill_id": state_bill_id,
+        "history": [actions]
+    }
+
+    return bill_history_data
 
 def collect_bill_data_1997_2001(uuid, state_bill_id, session_year):
     """
     Base function to collect data for sessions 1997-2001.
     Returns four JSON objects: bill_metadata, sponsors, bill_history, and votes.
     """
+
+    history_data = get_bill_history_1997_2001(uuid, session_year, state_bill_id)
+
     bill_meta_data, sponsors = get_bill_metadata_1997_2001(uuid, session_year, state_bill_id)
     write_file(uuid, "bill_metadata", bill_meta_data)
     write_file(uuid, "sponsors", sponsors)
 
-    history_data = get_bill_history_1997_2001(session_year, state_bill_id)    
-    
     bill_history = {
         "uuid": uuid,
         "state": "UT",
@@ -133,34 +176,6 @@ def collect_bill_data_1997_2001(uuid, state_bill_id, session_year):
         "date": history_data[0],
         "action": history_data[1]
     }
-
-def write_file(file_name, directory, data):
-    with open(f'output/{directory}/{file_name}.json', 'w') as f:
-        json.dump(data, f, indent=4)
-
-def setup_driver():
-    # Set up Chrome options
-    chrome_options = Options()
-
-    # Specify the download directory and enable automatic downloads
-    prefs = {
-        "download.prompt_for_download": False,
-        "download.directory_upgrade": True,
-        "safebrowsing.enabled": True
-    }
-    chrome_options.add_experimental_option("prefs", prefs)
-
-    # Add headless options (if needed)
-    chrome_options.add_argument("--headless")  # Runs Chrome in headless mode (no UI)
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-
-    # Initialize the Selenium webdriver with Chrome options
-    # Using webdriver_manager to handle the ChromeDriver automatically
-    service = Service(ChromeDriverManager().install())  # This will install and manage the ChromeDriver
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-
-    return driver
 
 # Example usage:
 if __name__ == "__main__":
