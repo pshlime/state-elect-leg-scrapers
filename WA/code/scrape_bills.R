5fee8f0bdbc090b78a52dfb2451ec79e90e9e4dd
Joe Loffredo <joseph.r.loffredo@gmail.com>
2025-04-10 20:42:45 -0400

    bill history

--- WA/code/scrape_bills.R
+++ WA/code/scrape_bills.R
@@ -28,7 +28,6 @@ get_bill_metadata <- function(UUID, session, bill_number){
   
   # Extract metadata from GetLegislation
   payload <- list(biennium = session, billNumber = bill_number)
-  
   response <- POST(url = "https://wslwebservices.leg.wa.gov/LegislationService.asmx/GetLegislation", body = payload, encode = "form")
   
   if (status_code(response) == 200) {
@@ -62,9 +61,8 @@ get_bill_metadata <- function(UUID, session, bill_number){
 }
 
 get_sponsors <- function(UUID, session, bill_number){
-  # Extract metadata from GetLegislation
+  # Extract metadata from GetSponsors
   payload <- list(biennium = session, billId = bill_number)
-  
   response <- POST(url = "https://wslwebservices.leg.wa.gov/LegislationService.asmx/GetSponsors", body = payload, encode = "form")
   
   if (status_code(response) == 200) {
@@ -109,6 +107,90 @@ get_sponsors <- function(UUID, session, bill_number){
     return(NULL)
   }
   
+}
+
+get_bill_history <- function(UUID, session, bill_number){
+  begin_date <- switch(
+    session,
+    '1995-96' = '1995-01-01',
+    '1997-98' = '1997-01-01',
+    '1999-00' = '1999-01-01',
+    '2001-02' = '2001-01-01',
+    '2003-04' = '2003-01-01',
+    '2005-06' = '2005-01-01',
+    '2007-08' = '2007-01-01',
+    '2009-10' = '2009-01-01',
+    '2011-12' = '2011-01-01',
+    '2013-14' = '2013-01-01',
+    '2015-16' = '2015-01-01',
+    '2017-18' = '2017-01-01',
+    '2019-20' = '2019-01-01',                   
+    '2021-22' = '2021-01-01',
+    '2023-24' = '2023-01-01'
+  )
+  
+  end_date <- switch(
+    session,
+    '1995-96' = '1996-12-31',
+    '1997-98' = '1998-12-31',
+    '1999-00' = '2000-12-31',
+    '2001-02' = '2002-12-31',
+    '2003-04' = '2004-12-31',
+    '2005-06' = '2006-12-31',
+    '2007-08' = '2008-12-31',
+    '2009-10' = '2010-12-31',
+    '2011-12' = '2012-12-31',
+    '2013-14' = '2014-12-31',
+    '2015-16' = '2016-12-31',
+    '2017-18' = '2018-12-31',
+    '2019-20' = '2020-12-31',                   
+    '2021-22' = '2022-12-31',
+    '2023-24' = '2024-12-31'
+  )
+  
+  # Extract metadata from GetLegislation
+  payload <- list(biennium = session, billNumber = bill_number, beginDate = begin_date, endDate = end_date)
+  response <- POST(url = "https://wslwebservices.leg.wa.gov/LegislationService.asmx/GetLegislativeStatusChangesByBillNumber", body = payload, encode = "form")
+  
+  if (status_code(response) == 200) {
+    history <- content(response, as = "parsed")
+    history_nodes <- xml_find_all(history, "//d1:ArrayOfLegislativeStatus/d1:LegislativeStatus", ns = xml_ns(history))
+    history <- as_list(history_nodes)
+    
+    flatten_history <- function(history) {
+      list(
+        BillId = history$BillId[[1]],
+        HistoryLine = history$HistoryLine[[1]],
+        ActionDate = history$ActionDate[[1]],
+        AmendedByOppositeBody = history$AmendedByOppositeBody[[1]],
+        PartialVeto = history$PartialVeto[[1]],
+        Veto = history$Veto[[1]],
+        AmendmentsExist = history$AmendmentsExist[[1]],
+        Status = history$Status[[1]]
+      )
+    }
+    
+    history_df <- map_dfr(history, flatten_history) |>
+      mutate(ActionDate = as_date(ActionDate))
+    
+    tibble(
+      uuid = UUID, 
+      state = 'WA', 
+      session = session, 
+      state_bill_id = bill_number, 
+      date = history_df$ActionDate,
+      action = history_df$HistoryLine
+    ) |> 
+      group_by(uuid, state, session, state_bill_id) |>
+      nest(history = c(date, action)) |>
+      ungroup() |>
+      as.list() |>
+      toJSON(pretty = T,auto_unbox = T) |> 
+      writeLines(glue("{OUTPUT_PATH}/bill_history/{UUID}.json"))
+  } else {
+    message("error getting bill history")
+    return(NULL)
+  }
 }
 
 scrape_bill <- function(UUID, session = NA, bill_number = NA){
