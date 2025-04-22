30531ddc187a2a19571e1fb3413c91dda1091401
Joe Loffredo <joseph.r.loffredo@gmail.com>
2025-04-22 18:49:18 -0400

    patch fixes after testing

--- FL/code/scrape_bills.R
+++ FL/code/scrape_bills.R
@@ -13,6 +13,8 @@ library(xml2)
 library(rvest)
 library(pdftools)
 library(tesseract)
+library(janitor)
+library(fs)
 
 rm(list = ls())
 gc()
@@ -48,9 +50,14 @@ clean_html <- function(html_content) {
   html_content <- str_replace_all(html_content, "</u> <u>", " ")
   html_content <- str_replace_all(html_content, "</u><u>", " ")
   html_content <- str_replace_all(html_content, '<u>', '<u class="amendmentInsertedText">')
+  html_content <- str_replace_all(html_content, '<u class=\"Insert\">', '<u class="amendmentInsertedText">')
   html_content <- str_replace_all(html_content, '<s>', '<strike class="amendmentDeletedText">')
+  html_content <- str_replace_all(html_content, '<s class="Remove">', '<strike class="amendmentDeletedText">')
   html_content <- str_replace_all(html_content, '</s>', '</strike>')
-  html_content <- str_replace_all(html_content, ' </pre>', ' ')
+  html_content <- str_replace_all(html_content, '</pre>', ' ')
+  html_content <- str_replace_all(html_content, "</font>","")
+  html_content <- str_replace_all(html_content, "<font[^>]*>", "")
+  html_content <- str_replace_all(html_content, "</page>", "")
   
   html_content <- str_trim(html_content)
   return(html_content)
@@ -91,7 +98,7 @@ get_bill_metadata <- function(UUID, session, bill_number, state_url, state_html)
 }
 
 get_sponsors <- function(UUID, session, bill_number, state_html){
-  sponsors_list <- state_html |> html_nodes("h2+ p") |> html_text() |> str_trim() |> str_squish()
+  sponsors_list <- state_html |> html_node("h2+ p") |> html_text() |> str_trim() |> str_squish()
   sponsors_list <- str_split(sponsors_list, ";")[[1]]
   sponsors_list <- str_trim(sponsors_list)
   cosponsor_index <- which(str_detect(sponsors_list, "\\(CO-INTRODUCERS\\)"))
@@ -164,7 +171,7 @@ get_bill_history <- function(UUID, session, bill_number, state_html){
 
 get_votes <- function(UUID, session, bill_number, state_html){
   # Check to see if any votes
-  if(is_empty(state_html |> html_node("#tabBodyVoteHistory") |> html_table())){
+  if(is_empty(state_html |> html_node("h4#Votes + table.tbl"))){
     return(NULL)
   } else{
     votes_df <- state_html |> html_node("h4#Votes + table.tbl") |> html_table(fill = TRUE)
@@ -207,6 +214,7 @@ get_votes <- function(UUID, session, bill_number, state_html){
           str_detect(rollcall_html, "Reading Number .: 3") ~ "Third Reading Passage",
           str_detect(rollcall_html, "PASSAGE") & str_detect(rollcall_html, "R2") ~ "Second Reading Passage",
           str_detect(rollcall_html, "PASSAGE") & str_detect(rollcall_html, "R2") ~ "Second Reading Passage",
+          str_detect(rollcall_html, "Floor Actions ..: Passage") ~ "Passage",
           str_detect(rollcall_html, "PASSAGE") ~ "Passage",
           TRUE ~ NA_character_
         ) 
@@ -263,12 +271,14 @@ get_votes <- function(UUID, session, bill_number, state_html){
             other = sum(!(response %in% c('Yea','Nay')), na.rm = TRUE)
           )
         
+        file_delete(dir_ls(glue("FL/output/scratch_files/")))
+        
         return(list(description = description, vote_data = vote_data, counts = counts))
       }
     }
     
     votes_df$roll_call <- map(votes_df$rollcall_url, parse_rollcall)
-    votes_df2 <- unnest_wider(votes_df, 'roll_call') |>
+    votes_df <- unnest_wider(votes_df, 'roll_call') |>
       unnest_wider('counts') |>
       mutate(
         uuid = UUID,
@@ -294,25 +304,63 @@ get_votes <- function(UUID, session, bill_number, state_html){
 }
 
 get_bill_text <- function(UUID, state_html){
+  bill_text_info <- state_html |> html_node("#tabBodyBillText") |> html_table(fill = T) |> 
+    select(Version, Posted) |>
+    clean_names() |>
+    mutate(
+      posted = mdy_hm(posted) |> format("%Y-%m-%d"),
+      UUID = UUID) |>
+      select(UUID, everything())
   
+  bill_text_links <- state_html |> html_nodes("#tabBodyBillText .lnk_BillTextHTML") |> html_attr("href")
+  bill_text_links <- glue("https://www.flsenate.gov{bill_text_links}")
+  bill_text_info$text_url <- bill_text_links
+  
+  process_text <- function(UUID, version, posted, text_url){
+    dir_create(glue("{OUTPUT_PATH}/bill_text/{UUID}"))
+    
+    download_path <- glue("{OUTPUT_PATH}/bill_text/{UUID}")
+    raw_file_name <- str_c(version, posted, sep = "_") |> 
+      str_replace_all(c("-"="_"," "="")) |> 
+      str_c("_plain",".txt")
+    html_file_name <- str_c(version, posted, sep = "_") |> 
+      str_replace_all(c("-"="_"," "="")) |> 
+      str_c("_html",".txt")
+    
+    download.file(text_url, glue("{download_path}/{raw_file_name}"), mode = "wb")
+    
+    text_html <- read_html(glue("{download_path}/{raw_file_name}"))
+    
+    raw_text <- text_html |> 
+      html_nodes("pre") |> 
+      html_text() |> 
+      str_remove_all("\r\n\r\n[0-9\\s]{1,3}") |>
+      str_remove_all("CODING: Words stricken are deletions; words underlined are additions.") |>
+      str_trim() |> 
+      str_squish()
+    
+    html_format <- text_html |> 
+      html_nodes("pre") |> 
+      as.character() |> 
+      str_remove_all("<a name=\"Page[0-9]{1,3}Line\\d+\"></a>\\s*\\d+\\s*") |>
+      str_remove_all("\r\n\r\n[0-9\\s]{1,3}") |>
+      clean_html() |>
+      str_remove_all("CODING: Words <strike class=\"amendmentDeletedText\">stricken</strike> are deletions; words <u class=\"amendmentInsertedText\">underlined</u> are additions.") |>
+      str_trim() |> 
+      str_squish()
+    
+    # Debugging logs
+    message("Writing plain text file: ", raw_file_name)
+    writeLines(raw_text, glue("{download_path}/{raw_file_name}"))
+    
+    message("Writing HTML format text file: ", html_file_name)
+    writeLines(html_format, glue("{download_path}/{html_file_name}"))
+  }
+  
+  pmap(bill_text_info, process_text)
 }
 
 scrape_bill <- function(UUID, session = NA, bill_number = NA){
-  session <- as.character(session)
-  session <- case_when(
-    str_detect(bill_number, "HB|HJR") & session == '2002' ~ '4',
-    str_detect(bill_number, "HB|HJR") & session == '2003' ~ '28',
-    str_detect(bill_number, "HB|HJR") & session == '2004' ~ '36',
-    str_detect(bill_number, "HB|HJR") & session == '2005' ~ '38',
-    str_detect(bill_number, "HB|HJR") & session == '2006' ~ '42',
-    str_detect(bill_number, "HB|HJR") & session == '2007' ~ '54',
-    str_detect(bill_number, "HB|HJR") & session == '2008' ~ '57',
-    str_detect(bill_number, "HB|HJR") & session == '2009' ~ '61',
-    str_detect(bill_number, "HB|HJR") & session == '2010' ~ '64',
-    str_detect(bill_number, "HB|HJR") & session == '2011' ~ '66',
-    TRUE ~ session
-  )
-  
   state_url <- build_url(session, bill_number)
   state_html <- read_html(state_url)
   
@@ -324,9 +372,7 @@ scrape_bill <- function(UUID, session = NA, bill_number = NA){
 }
 
 # ## Testing
-# session = '2021-22'
-# UUID = 'WA2021SB5015'
-# bill_number = 5015
+# session = '2007'
+# UUID = 'FL2007H537'
+# bill_number = 'HB537'
 # scrape_bill(UUID, session, bill_number)
-
-
