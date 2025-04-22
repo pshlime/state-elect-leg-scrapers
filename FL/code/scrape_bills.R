97cc16f6cd0c4cedf55845ca31df636685546661
Joe Loffredo <joseph.r.loffredo@gmail.com>
2025-04-22 13:01:41 -0400

    move house bills 1998-2001 to senate site

--- FL/code/scrape_bills.R
+++ FL/code/scrape_bills.R
@@ -37,7 +37,7 @@ HEADERS <- c(
 
 # Functions
 build_url <- function(session, bill_number){
-  if(str_detect(bill_number, "SJR|SB")){
+  if(str_detect(bill_number, "SJR|SB") | str_detect(bill_number, "HB|HJR") & session %in% c("48", "49", "50", "51", "71", "72")){
     # Senate bills
     # Pull just number
     bill_number <- str_extract(bill_number, "\\d+")
@@ -77,14 +77,10 @@ get_bill_metadata <- function(UUID, session, bill_number, state_url, state_html)
     status <- ifelse(str_detect(status, "Chapter"), glue("Enacted - {status}"), status)
     
   } else if(str_detect(state_url, "house")){
-    if(session %in% c("48", "49", "50", "51", "71", "72")){
-      return(NULL)
-    } else{
-      title <- state_html |> html_nodes("#header-bill-subject") |> html_text() |> str_squish()
-      description <- state_html |> html_nodes("#bill-subject") |> html_text() |> str_squish()
-      status <- state_html |> html_nodes("#lblLastAction") |> html_text() |> str_squish()
-      status <- ifelse(str_detect(status, "Chapter"), glue("Enacted - {status}"), status)
-    }
+    title <- state_html |> html_nodes("#header-bill-subject") |> html_text() |> str_squish()
+    description <- state_html |> html_nodes("#bill-subject") |> html_text() |> str_squish()
+    status <- state_html |> html_nodes("#lblLastAction") |> html_text() |> str_squish()
+    status <- ifelse(str_detect(status, "Chapter"), glue("Enacted - {status}"), status)
   }
     tibble(
       uuid = UUID, 
@@ -98,7 +94,7 @@ get_bill_metadata <- function(UUID, session, bill_number, state_url, state_html)
     ) |> as.list() |> toJSON(auto_unbox = T, pretty = T) |> writeLines(glue("{OUTPUT_PATH}/bill_metadata/{UUID}.json"))
 }
 
-get_sponsors <- function(UUID, session, bill_number, state_html){
+get_sponsors <- function(UUID, session, bill_number, state_url, state_html){
   if(str_detect(state_url, "senate")){
     sponsors_list <- state_html |> html_nodes("#main > div > div.grid-100 > p:nth-child(5)") |> html_text() |> str_trim() |> str_squish()
     sponsors_list <- str_split(sponsors_list, ";")[[1]]
@@ -123,15 +119,11 @@ get_sponsors <- function(UUID, session, bill_number, state_html){
       sponsors_df <- data.frame(sponsor_name = c(sponsors), sponsor_type = "sponsor")
     }
   } else if(str_detect(state_url, "house")){
-    if(session %in% c("48", "49", "50", "51", "71", "72")){
-      return(NULL)
-    } else{
-      sponsors <- state_html |> 
-        html_nodes("#lblSponsors") |> 
-        html_text() |> 
-        str_squish()
-      sponsors_df <- data.frame(sponsor_name = c(sponsors), sponsor_type = "sponsor")
-    }
+    sponsors <- state_html |> 
+      html_nodes("#lblSponsors") |> 
+      html_text() |> 
+      str_squish()
+    sponsors_df <- data.frame(sponsor_name = c(sponsors), sponsor_type = "sponsor")
   }
   
   tibble(
@@ -150,15 +142,29 @@ get_sponsors <- function(UUID, session, bill_number, state_html){
     writeLines(glue("{OUTPUT_PATH}/sponsors/{UUID}.json"))
 }
 
-get_bill_history <- function(UUID, session, bill_number){
+get_bill_history <- function(UUID, session, bill_number, state_url, state_html){
+  if(str_detect(state_url, "senate")){
+    history_df <- state_html |>
+      html_node("#tabBodyBillHistory") |>
+      html_table(fill = T) |>
+      mutate(
+        date = as_date(Date, format = "%m/%d/%Y"),
+        Action = str_remove_all(Action, "• ") |> str_trim() |> str_squish(),
+        action = glue("{Chamber} - {Action}"),
+        action = str_replace_all(action, c("^Senate - "="S - ","House - "="H - "))
+      ) |>
+      select(date, action)
+  } else if(str_detect(state_url, "house")){
     
+  }
+  
   tibble(
     uuid = UUID, 
     state = 'FL', 
     session = session, 
     state_bill_id = bill_number, 
-    date = history_df$ActionDate,
-    action = history_df$HistoryLine
+    date = history_df$date,
+    action = history_df$action
   ) |> 
     group_by(uuid, state, session, state_bill_id) |>
     nest(history = c(date, action)) |>
@@ -169,54 +175,11 @@ get_bill_history <- function(UUID, session, bill_number){
 }
 
 get_votes <- function(UUID, session, bill_number){
-  # Extract metadata from GetRollCalls
-  payload <- list(biennium = session, billNumber = bill_number)
-  response <- POST(url = "https://wslwebservices.leg.wa.gov/LegislationService.asmx/GetRollCalls", body = payload, encode = "form")
-  
-  if (status_code(response) == 200) {
-    votes <- content(response, as = "parsed")
-    votes_nodes <- xml_find_all(votes, "//d1:ArrayOfRollCall/d1:RollCall", ns = xml_ns(votes))
-    votes <- as_list(votes_nodes)
-    
-    flatten_votes <- function(votes) {
-      metadata <- list(
-        Agency = votes$Agency[[1]],
-        BillId = votes$BillId[[1]],
-        Biennium = votes$Biennium[[1]],
-        Motion = votes$Motion[[1]],
-        SequenceNumber = votes$SequenceNumber[[1]],
-        VoteDate = votes$VoteDate[[1]],
-        YeaVotes = as.numeric(votes$YeaVotes$Count[[1]]),
-        NayVotes = as.numeric(votes$NayVotes$Count[[1]]),
-        AbsentVotes = as.numeric(votes$AbsentVotes$Count[[1]]),
-        ExcusedVotes = as.numeric(votes$ExcusedVotes$Count[[1]])
-      )
-      # extract member-level votes
-      member_votes <- map_dfr(votes$Votes, function(v) {
-        tibble(
-          Name = v$Name,
-          Vote = v$VOte
-        )
-      }) |>
-        mutate(
-          Vote = case_when(
-            Vote == 'Yea' ~ 'Yea',
-            Vote == 'Nay' ~ 'Nay',
-            Vote == 'Absent' ~ 'Absent',
-            TRUE ~ 'NV'
-          )
-        ) |>
-        rename(name = Name, response = Vote)
-      
-      # add roll_call as list-column
-      metadata$roll_call <- list(member_votes)
-      return(metadata)
-    }
     
     votes_df <- map_dfr(votes, flatten_votes) |>
       mutate(
         uuid = UUID,
-        state = 'WA',
+        state = 'FL',
         session = session,
         state_bill_id = bill_number,
         chamber = case_match(
@@ -240,24 +203,11 @@ get_votes <- function(UUID, session, bill_number){
         toJSON(pretty = T, auto_unbox = T) |>
         writeLines(glue("{OUTPUT_PATH}/votes/{UUID}_{i}.json"))
     }
-  } else {
-    message("error getting sponsors")
-    return(NULL)
-  }
 }
 
 scrape_bill <- function(UUID, session = NA, bill_number = NA){
   session <- as.character(session)
   session <- case_when(
-    str_detect(bill_number, "SB|SJR") ~ session,
-    str_detect(bill_number, "HB|HJR") & session == '1998' ~ '48',
-    str_detect(bill_number, "HB|HJR") & session == '1999' ~ '49',
-    str_detect(bill_number, "HB|HJR") & session == '2000A' ~ '50',
-    str_detect(bill_number, "HB|HJR") & session == '2000' ~ '51',
-    str_detect(bill_number, "HB|HJR") & session == '2001A' ~ '71',
-    str_detect(bill_number, "HB|HJR") & session == '2001' ~ '72',
-    str_detect(bill_number, "HB|HJR") & session == '2001B' ~ '5',
-    str_detect(bill_number, "HB|HJR") & session == '2001C' ~ '8',
     str_detect(bill_number, "HB|HJR") & session == '2002' ~ '4',
     str_detect(bill_number, "HB|HJR") & session == '2003' ~ '28',
     str_detect(bill_number, "HB|HJR") & session == '2004' ~ '36',
@@ -271,14 +221,14 @@ scrape_bill <- function(UUID, session = NA, bill_number = NA){
   )
   
   state_url <- build_url(session, bill_number)
-  if (str_detect(bill_number, "SB|SJR")) {
+  if (str_detect(bill_number, "SB|SJR") | str_detect(bill_number, "HB|HJR") & session %in% c("1998","1999","2000","2001","2001A")) {
     state_html <- read_html(state_url)
   } else if (str_detect(bill_number, "HB|HJR")) {
     state_html <- GET(state_url, add_headers(.headers = HEADERS)) |> content("text") |> read_html()
   }
   
   get_bill_metadata(UUID, session, bill_number, state_url, state_html)
-  get_sponsors(UUID, session, bill_number, state_html)
+  get_sponsors(UUID, session, bill_number, state_url, state_html)
   get_bill_history(UUID, session, bill_number)
   get_votes(UUID, session, bill_number)
 }
