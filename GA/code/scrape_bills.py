3b7a4ba2c9934f1fc394f40cd1db11abd910cb28
Joe Loffredo <joseph.r.loffredo@gmail.com>
2025-05-08 14:59:52 -0400

    retrieve bill_text links

--- GA/code/scrape_bills.py
+++ GA/code/scrape_bills.py
@@ -95,6 +95,8 @@ action_code_map = {
     "HTS": None,
 }
 
+vote_name_pattern = re.compile(r"(.*), (\d+(?:ST|ND|RD|TH))", re.IGNORECASE)
+
 url = "http://webservices.legis.ga.gov/GGAServices/%s/Service.svc?wsdl"
 
 
@@ -165,9 +167,13 @@ def write_file(file_name, directory, data):
     with open(f'GA/output/{directory}/{file_name}.json', 'w') as f:
         json.dump(data, f, indent=4)
 
+def get_member(mservice, member_id):
+    mem = backoff(mservice.GetMember, member_id)
+    return f"{mem.Name['FamilyName']}, {mem.Name['First']}"
+
 def get_bill_metadata(uuid, session, instrument):
     """
-    Get bill metadata from the instrument object and save it to a CSV file.
+    Get bill metadata from the instrument object.
     """
     metadata = {
         "uuid": uuid,
@@ -184,7 +190,7 @@ def get_bill_metadata(uuid, session, instrument):
 
 def get_bill_sponsors(uuid, session, authors):
     """
-    Get bill sponsors from the instrument object and save it to a CSV file.
+    Get bill sponsors from the instrument object.
     """
     sponsors = []
     for sponsor in authors[0]:
@@ -216,7 +222,7 @@ def get_bill_history(uuid, session, status_history):
     """
     history = []
     for status in status_history:
-        date = status["Date"].date()
+        date = status["Date"].date().strftime("%Y-%m-%d")
         action = f"{action_code_map[status["Code"]]} - {status["Description"]}"
 
         history.append({
@@ -233,34 +239,101 @@ def get_bill_history(uuid, session, status_history):
         }
     return history_data
 
-
-lservice = get_client("Legislation").service
-vservice = get_client("Votes").service
-mservice = get_client("Members").service
-lsource = get_url("Legislation")
-msource = get_url("Members")
-vsource = get_url("Votes")
-
-text_links = []
-bill_list = pd.read_csv("GA/output/ga_legislation_by_session_merged.csv")
-
-for s in ['2001_02', '2003_04', '2005_06', '2007_08', '2009_10', '2011_12', '2013_14']:
-    session_bills = bill_list[bill_list["session"] == s]
-    sid = SESSION_SITE_IDS[s]
-    legislation = backoff(lservice.GetLegislationForSession, sid)["LegislationIndex"]
+def get_votes(uuid, session, votes):
+    """
+    Get votes from the instrument object.
+    """
+    for vote in votes:
+        vote_details = backoff(vservice.GetVote, vote["VoteId"])
     
-    for index, row in session_bills.iterrows():
-        UUID = row["UUID"]
-        api_id = row["ga_id"]
+        roll_call = vote_details.Votes["MemberVote"]
+        roll_call_records = []
+        for r in roll_call:
+            member_id = r['Member']['Id']
+            name = get_member(mservice, member_id)
+            response = r['MemberVoted']
+            response = "NV" if response == "NotVoting" else str(r['MemberVoted'])
 
-        instrument = backoff(lservice.GetLegislationDetail, api_id)
+            roll_call_records.append({
+                "name": name,
+                "response": response
+            })
 
-        bill_metadata = get_bill_metadata(UUID, s, instrument)
-        write_file(UUID, "bill_metadata", bill_metadata)
+        votes_data = {
+            "uuid": uuid,
+            "state": "GA",
+            "session": session,
+            "state_bill_id": f"{instrument.DocumentType} {instrument.Number}",
+            "chamber": "H" if vote_details.Branch == "House" else "S",
+            "date": vote_details.Date.date().strftime("%Y-%m-%d"),
+            "description": f"{vote_details.Caption}-{vote_details.Description}",
+            "yeas": vote_details.Yeas,
+            "nays": vote_details.Nays,
+            "other": vote_details.Excused + vote_details.NotVoting,
+            "roll_call": [roll_call_records]
+        }
 
-        sponsors = get_bill_sponsors(UUID, s, instrument.Authors)
-        write_file(UUID, "sponsors", sponsors)
+        file_name = f"{uuid}_{vote_details.VoteId}"
+        write_file(file_name, "votes", votes_data)
 
-        bill_history = get_bill_history(UUID, s, instrument.StatusHistory)
-        write_file(UUID, "bill_history", bill_history)
-        print('test')
+
+def get_bill_text_link(uuid, versions):
+    """
+    Get bill text link from the instrument object.
+    Returns a 1-row DataFrame with uuid, text_url, and text_version.
+    """
+    links = pd.DataFrame(columns=['uuid', 'text_url', 'text_version'])
+    for version in versions:
+        text_url = version['Url']
+        text_version = version['Version']
+
+        # Create 1-row DataFrame
+        version_text = pd.DataFrame([{
+            'uuid': uuid,
+            'text_url': text_url,
+            'text_version': text_version
+        }])
+
+        links = pd.concat([links, version_text], ignore_index=True)
+
+    # In case versions is empty
+    return links
+
+if __name__ == "__main__":
+    lservice = get_client("Legislation").service
+    vservice = get_client("Votes").service
+    mservice = get_client("Members").service
+    lsource = get_url("Legislation")
+    msource = get_url("Members")
+    vsource = get_url("Votes")
+
+    text_links = pd.DataFrame(columns=['uuid', 'text_url', 'text_version'])
+    bill_list = pd.read_csv("GA/output/ga_legislation_by_session_merged.csv")
+
+    for s in ['2001_02', '2003_04', '2005_06', '2007_08', '2009_10', '2011_12', '2013_14']:
+        session_bills = bill_list[bill_list["session"] == s]
+        sid = SESSION_SITE_IDS[s]
+        legislation = backoff(lservice.GetLegislationForSession, sid)["LegislationIndex"]
+        
+        for index, row in session_bills.iterrows():
+            UUID = row["UUID"]
+            api_id = row["ga_id"]
+
+            instrument = backoff(lservice.GetLegislationDetail, api_id)
+
+            bill_metadata = get_bill_metadata(UUID, s, instrument)
+            write_file(UUID, "bill_metadata", bill_metadata)
+
+            sponsors = get_bill_sponsors(UUID, s, instrument.Authors)
+            write_file(UUID, "sponsors", sponsors)
+
+            bill_history = get_bill_history(UUID, s, instrument.StatusHistory['StatusListing'])
+            write_file(UUID, "bill_history", bill_history)
+
+            if instrument.Votes is not None and 'VoteListing' in instrument.Votes:
+                votes = instrument.Votes['VoteListing']
+                get_votes(UUID, s, votes)
+
+            text_links = pd.concat([text_links, get_bill_text_link(UUID, instrument.Versions['DocumentDescription'])], ignore_index=True)
+
+    text_links.to_csv("GA/output/ga_bill_text_links.csv", index=False)
