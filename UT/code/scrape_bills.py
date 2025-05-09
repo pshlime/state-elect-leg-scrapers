1cb64c1d977e954a7ce80706c7340cdf0f08bd90
Joe Loffredo <joseph.r.loffredo@gmail.com>
2025-05-09 11:22:42 -0400

    rename UT scrape_bills

--- UT/code/scrape_bills.py
+++ UT/code/scrape_bills.py
@@ -39,17 +39,20 @@ def get_status(soup):
             return flags[flag].format(last_action=last_action)
     return last_action
     
-#bill functions
 
+#bill functions
 def get_bill_metadata_1997_2001(uuid, session_year, state_bill_id):
     """
     Retrieves the bill title and sponsor from the bill metadata page.
     """
     # Construct the URL for the bill's HTML page.
-    if session_year == "1997":
-        url = f"https://le.utah.gov/~{session_year}/htmdoc/hbillhtm/{state_bill_id}.htm"
+    if state_bill_id.upper().startswith(("SB", "SR", "SJR")):
+        bill_folder = "sbillhtm" if session_year == "1997" else "Sbillhtm"
     else:
-        url = f"https://le.utah.gov/~{session_year}/htmdoc/Hbillhtm/{state_bill_id}.htm"
+        bill_folder = "hbillhtm" if session_year == "1997" else "Hbillhtm"
+
+    url = f"https://le.utah.gov/~{session_year}/htmdoc/{bill_folder}/{state_bill_id}.htm"
+    
     to_scrape = requests.get(url)
     soup = BeautifulSoup(to_scrape.content, 'html.parser')
     
@@ -86,7 +89,67 @@ def get_bill_metadata_1997_2001(uuid, session_year, state_bill_id):
     }
     
     return bill_metadata,sponsors_output
-       
+  
+def get_bill_metadata_2002(uuid, session_year, state_bill_id):
+    """
+    Retrieves the bill title and sponsor from the bill metadata page.
+    """
+    # Construct the URL for the bill's HTML page.
+    url = f"https://le.utah.gov/~{session_year}/bills/static/{state_bill_id}.html"
+    to_scrape = requests.get(url)
+    soup = BeautifulSoup(to_scrape.content, 'html.parser')
+
+    #get title
+    header_elt = soup.find('h3')
+    header = header_elt.get_text(strip=True).split()[2:] if header_elt else None
+    title = " ".join(header)
+
+    #get sponsor
+    bill_sponsor_div = soup.find('div', id='billsponsordiv')
+    floor_sponsor_div = soup.find('div', id='floorsponsordiv')
+    sponsor1 = bill_sponsor_div.get_text(strip=True).replace("Bill Sponsor:", "") if bill_sponsor_div else None
+    sponsor2 = floor_sponsor_div.get_text(strip=True).replace("Floor Sponsor:", "") if floor_sponsor_div else None
+ 
+    #get status
+    last_action_li = soup.select_one('ul.billinfoulm li:contains("Last Action")')
+    last_action = re.search(r"Last Action:.*?(?=Last Location)", last_action_li.get_text(strip=True))
+    last_action = str(last_action) # added this because of an error i was getting, i think it's fixed?
+    last_action = last_action.replace("Last Action:", "").strip()
+    
+    status = "Enacted - {last_action}".format(last_action = last_action) if any("Signed" in chunk for chunk in last_action) else last_action
+
+    bill_metadata = {
+        "uuid": uuid,
+        "state": "UT",
+        "session": session_year,  
+        "state_bill_id": state_bill_id,
+        "title": title,
+        "description": None, 
+        "status": status,
+        "state_url": url
+    }
+
+    sponsors = []
+    sponsors.append({
+        "sponsor_name": sponsor1.replace("Rep. ", "").replace("Sen. ", ""),
+        "sponsor_type": "sponsor",
+    })
+    if sponsor2:
+        sponsors.append({
+            "sponsor_name": sponsor2.replace("Rep. ", "").replace("Sen. ", ""),
+            "sponsor_type": "sponsor",
+        })
+
+    sponsors_output = {
+        "uuid": uuid,
+        "state": "UT",
+        "session": session_year,
+        "state_bill_id": state_bill_id,
+        "sponsors": sponsors
+    }
+    
+    return bill_metadata,sponsors_output
+          
 
 def get_bill_history_1997_2001(uuid, session_year, state_bill_id):
     """
@@ -94,7 +157,11 @@ def get_bill_history_1997_2001(uuid, session_year, state_bill_id):
     descriptions, and vote counts (if available).
     """
     # Construct the URL for the status text file.
-    status_url = f"https://le.utah.gov/~{session_year}/status/hbillsta/{state_bill_id}.txt"
+    if state_bill_id.upper().startswith(("SB", "SR", "SJR")):
+        status_folder = "sbillsta"
+    else:
+        status_folder = "hbillsta"
+    status_url = f"https://le.utah.gov/~{session_year}/status/{status_folder}/{state_bill_id}.txt"
     
     response = requests.get(status_url)
     text_data = response.text
@@ -112,22 +179,128 @@ def get_bill_history_1997_2001(uuid, session_year, state_bill_id):
     }
     return bill_history
 
+def get_bill_history_2002(uuid, session_year, state_bill_id):
+    """
+    Retrieves the bill history from the status text file and extracts action dates,
+    descriptions, and vote counts (if available).
+    """
+    if state_bill_id.upper().startswith(("SB", "SR", "SJR")):
+        status_folder = "sbillsta"
+    else:
+        status_folder = "hbillsta"
+
+    # Construct the status URL
+    status_url = f"https://le.utah.gov/~{session_year}/status/{status_folder}/{state_bill_id}.txt"
+    
+    response = requests.get(status_url)
+    text_data = response.text
+   
+    pattern = r"(\d{2}/\d{2}/\d{2})\s+(.+)"
+    matches = re.findall(pattern, text_data)
+    actions = [{"date": convert_date(match[0]), "action": " ".join(match[1].split()[:-1])} for match in matches]
+    
+    print(actions)
+
+    bill_history = {
+        "uuid": uuid,
+        "state": "UT",
+        "session": session_year,
+        "state_bill_id": state_bill_id,
+        "bill_history": [actions]
+    }
+    return bill_history
+
+def get_votes(uuid,session_year,state_bill_id):
+    if state_bill_id.upper().startswith(("SB", "SR", "SJR")):
+        status_folder = "sbillsta"
+    else:
+        status_folder = "hbillsta"
+
+    url = f"https://le.utah.gov/~{session_year}/status/{status_folder}/{state_bill_id}.001h.txt"
+    
+    response = requests.get(url)
+    text_data = response.text.split()
+    
+    chamber = text_data[text_data.index(state_bill_id[-2:])+1][0]
+
+    #get description
+    months = {"JANUARY":"01","FEBRUARY":"02","MARCH":"03","APRIL":"04","MAY":"05","JUNE":"06","JULY":"07",
+              "AUGUST":"08","SEPTEMBER":"09","OCTOBER":"10","NOVEMBER":"11","DECEMBER":"12"}
+    desc_idx = text_data.index("TABULATION")+1
+    description = ""
+    while text_data[desc_idx] not in months.keys():
+        description += text_data[desc_idx] + " "
+        desc_idx += 1
+    
+    #get date
+    date = text_data[desc_idx+2] + "-" + months[text_data[desc_idx]] + "-" + text_data[desc_idx+1]
+
+    #vote info
+    yeas = text_data[text_data.index("YEAS")+2]
+    nays = text_data[text_data.index("NAYS")+2]
+    other = text_data[text_data.index("VOTING")+2]
+
+    #getting roll call
+    roll_call = []
+    roll_idx = text_data.index("YEAS")+3
+    r= "Yea"
+    while roll_idx <= len(text_data)-1:
+        if text_data[roll_idx] == "NAYS":
+            roll_idx += 3
+            r = "Nay"
+        if text_data[roll_idx] == "ABSENT":
+            roll_idx += 6
+            r = "Absent"
+        
+        roll_call.append({
+            "name": text_data[roll_idx],
+            "response": r
+        })
+        roll_idx +=1
+
+    votes = {
+        "uuid": uuid,
+        "state": "UT",
+        "session": session_year,  
+        "state_bill_id": state_bill_id,
+        "chamber": chamber,
+        "date": date, 
+        "description": description,
+        "yeas": yeas,
+        "nays":nays,
+        "other":other,
+        "roll_call": [roll_call]
+    }
+    return votes
+
 #main function
-def collect_bill_data_1997_2001(uuid, session_year, state_bill_id):
+def collect_bill_data(uuid, session_year, state_bill_id):
     """
     Base function to collect data for sessions 1997-2001.
     Returns four JSON objects: bill_metadata, sponsors, bill_history, and votes.
     """
-    metadata,sponsors = get_bill_metadata_1997_2001(uuid,session_year, state_bill_id)
-    write_file(uuid, "bill_metadata", metadata)
-    write_file(uuid, "sponsors", sponsors)
+    if int(session_year) >= 2002:
+        metadata,sponsors = get_bill_metadata_2002(uuid,session_year, state_bill_id)
+        write_file(uuid, "bill_metadata", metadata)
+        write_file(uuid, "sponsors", sponsors)
+            
+        history_data = get_bill_history_2002(uuid, session_year, state_bill_id)    
+        write_file(uuid, "bill_history", history_data)
+        votes = get_votes(uuid,session_year,state_bill_id)
+        write_file(uuid,"votes",votes)
+        return {"bill_metadata":metadata,"sponsors":sponsors,"bill_history":history_data,"votes":votes}
+    elif int(session_year) < 2002:
+        metadata,sponsors = get_bill_metadata_1997_2001(uuid,session_year, state_bill_id)
+        write_file(uuid, "bill_metadata", metadata)
+        write_file(uuid, "sponsors", sponsors)
+            
+        history_data = get_bill_history_1997_2001(uuid, session_year, state_bill_id)    
+        write_file(uuid, "bill_history", history_data)
+        return {"bill_metadata":metadata,"sponsors":sponsors,"bill_history":history_data}
+    print("Something's wrong!")
         
-    history_data = get_bill_history_1997_2001(uuid, session_year, state_bill_id)    
-    write_file(uuid, "bill_history", history_data)
-    return {"bill_metadata":metadata,"sponsors":sponsors,"bill_history":history_data}
-    
 
 # Example usage:
 if __name__ == "__main__":
-    # For example, collecting data for HB0417 from 1998.
-    bill_data = collect_bill_data_1997_2001("UT1998H417", "1998", "HB0417")
+    # bill_data = collect_bill_data_1997_2001("UT2008HB71", "2008", "HB0071")
+    print(collect_bill_data("UT2009S24", "2009", "SB0024"))
